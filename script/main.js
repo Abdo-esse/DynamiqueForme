0bd9528eea7c3abc69d17881d3ef1dd1afa6c7f2
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-14 00:03:07 +0100

    j'ai termine la fonction du ajout input work experience

--- script/main.js
+++ script/main.js
@@ -7,9 +7,32 @@ let inputPersone = document.querySelector('#inputUser')
 // parent of inputPerson
 let parentInputPerson = document.querySelector('.input-name')
 
+// input of workExperience
+let workExperience = document.querySelector('#workExperience')
+
+// add work Experience button
+let adWorkExperience = document.querySelector('#ad-work-experience')
+
+//parent of input of work experience
+let parentInputExperience = document.querySelector('#all')
+
  //function de add person nam
  function addPerson() {
     let newPerson = inputPersone.cloneNode(true)
     parentInputPerson.appendChild(newPerson)
  }
+
+ // Function de add work Experience
+ function addExperionce() {
+   let newExep = workExperience.cloneNode(true)
+   parentInputExperience.appendChild(newExep)
+}
+
+
+
+ // Declaration du fonction addPerson
  addPersoneBtn.addEventListener('click',addPerson)
+
+ // Declaration du fonction add work Experience
+ adWorkExperience.addEventListener('click',addExperionce)
+
