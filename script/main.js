b82ae02f5449d64c9e249371215468abd349a462
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-13 23:38:18 +0100

    j'ai declarer LES variablr

---
+++ script/main.js
@@ -0,0 +1,10 @@
+// name persone input
+let inputPersone = document.querySelector('#inputUser')
+
+// add persone button 
+ let addPersoneBtn = document.querySelector('#addpersone')
+
+ console.log(inputPersone);
+ console.log(addPersoneBtn);
+ 
+ 
