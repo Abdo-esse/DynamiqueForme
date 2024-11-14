5d795ecaebf8ebb5da0095872d8ea1fe1fe750bb
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-14 09:02:20 +0100

    j'ai commence la modification sur la  partie js

--- script/main.js
+++ script/main.js
@@ -1,69 +1,69 @@
-// name persone input
-let inputPersone = document.querySelector('#inputUser')
+// // name persone input
+// let inputPersone = document.querySelector('#inputUser')
 
-// add persone button 
- let addPersoneBtn = document.querySelector('#addpersone')
+// // add persone button 
+//  let addPersoneBtn = document.querySelector('#addpersone')
 
-// parent of inputPerson
-let parentInputPerson = document.querySelector('.input-name')
+// // parent of inputPerson
+// let parentInputPerson = document.querySelector('.input-name')
 
-// input of workExperience
-let workExperience = document.querySelector('#workExperience')
+// // input of workExperience
+// let workExperience = document.querySelector('#workExperience')
 
-// add work Experience button
-let adWorkExperience = document.querySelector('#ad-work-experience')
+// // add work Experience button
+// let adWorkExperience = document.querySelector('#ad-work-experience')
 
-//parent of input of work experience
-let parentInputExperience = document.querySelector('.all')
+// //parent of input of work experience
+// let parentInputExperience = document.querySelector('.all')
 
-// SaveData button
-let SaveData = document.querySelector("#saveData")
+// // SaveData button
+// let SaveData = document.querySelector("#saveData")
 
-//Data arry
-let data = []
-
-
+// //Data arry
+// let data = []
 
 
 
 
 
 
- //function de add person nam
- function addPerson() {
-
-   //Arry input Persone nam
-   let namePersones =Array.from(document.querySelectorAll('.namePerson'))
 
 
-    let newPerson = inputPersone.cloneNode(true)
-    parentInputPerson.appendChild(newPerson)
-    newPerson.id =''
-    newPerson.value =''
-    let datanemPerson = namePersones.map(x=>x.value)
+//  //function de add person nam
+//  function addPerson() {
+
+//    //Arry input Persone nam
+//    let namePersones =Array.from(document.querySelectorAll('.namePerson'))
+
+
+//     let newPerson = inputPersone.cloneNode(true)
+//     parentInputPerson.appendChild(newPerson)
+//     newPerson.id =''
+//     newPerson.value =''
+//     let datanemPerson = namePersones.map(x=>x.value)
     
    
    
- }
+//  }
 
- // Function de add work Experience
- function addExperionce() {
+//  // Function de add work Experience
+//  function addExperionce() {
    
-   let newExep = workExperience.cloneNode(true)
-   parentInputExperience.appendChild(newExep)
-   newExep.id=''
+//    let newExep = workExperience.cloneNode(true)
+//    parentInputExperience.appendChild(newExep)
+//    newExep.id=''
 
-}
+// }
 
-// Function de Aficher 
-function showModel() {
+// // Function de Aficher 
+// function showModel() {
  
-}
+// }
 
- // Declaration du fonction addPerson
- addPersoneBtn.addEventListener('click',addPerson)
+//  // Declaration du fonction addPerson
+//  addPersoneBtn.addEventListener('click',addPerson)
 
- // Declaration du fonction add work Experience
- adWorkExperience.addEventListener('click',addExperionce)
+//  // Declaration du fonction add work Experience
+//  adWorkExperience.addEventListener('click',addExperionce)
 
 
